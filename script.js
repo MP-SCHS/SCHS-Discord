342dea42162276a6db31e4da03f0498dbb7be4e9
MP-SCHS <28mpucci@salpointe.org>
2026-03-31 08:26:59 -0700

    v1

--- script.js
+++ script.js
@@ -1,11 +1,15 @@
 function message() {
-  // Request permission
-Notification.requestPermission().then(permission => {
-  if (permission === "granted") {
-    new Notification("Hello from the Website!", {
-      body: "This is a desktop alert.",
-      icon: "logo.png"
+  if (!("Notification" in window)) {
+    alert("This browser does not support desktop notifications");
+  } else if (Notification.permission === "granted") {
+    // If permission is already granted, just show it
+    new Notification("Hello!");
+  } else if (Notification.permission !== "denied") {
+    // Otherwise, ask for permission then show it
+    Notification.requestPermission().then(permission => {
+      if (permission === "granted") {
+        new Notification("Hello!");
+      }
     });
   }
-});
 }
